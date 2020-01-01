3896cbf8401f4fef332e7272a85beb2ff1f33451
Maciej Jedwabny <rudykot@gmail.com>
2020-01-01 22:48:19 +0100

    Day4 Part2

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,13 +1,48 @@
-module Day4 (meetsCriteria, passwordsMeetingCriteria) where
+module Day4
+  ( meetsCriteria
+  , passwordsMeetingCriteria
+  , meetsExtendedCriteria
+  , passwordsMeetingExtendedCriteria
+  ) where
+
+passwordsMeetingExtendedCriteria :: String -> Int
+passwordsMeetingExtendedCriteria range = do
+  let parts = wordsWhen (== '-') range
+  let x1 = parts !! 0
+  let x2 = parts !! 1
+  let p1 = read x1 :: Int
+  let p2 = read x2 :: Int
+  length [password | password <- [p1 .. p2], meetsExtendedCriteria (show password)]
+
+meetsExtendedCriteria :: String -> Bool
+meetsExtendedCriteria password =
+  length password == 6 &&
+  neverDecreases password &&
+  twoAdjacentDigitsAreSameAndNotPartOfBiggerGroup password
+
+twoAdjacentDigitsAreSameAndNotPartOfBiggerGroup :: String -> Bool
+twoAdjacentDigitsAreSameAndNotPartOfBiggerGroup password = do
+  let curried = adjacentDigitsAndNotPartOfBiggerGroup password
+  any curried (pairs password)
+
+adjacentDigitsAndNotPartOfBiggerGroup :: String -> (Char, Char) -> Bool
+adjacentDigitsAndNotPartOfBiggerGroup password pair =
+  adjacentDigits pair && 2 == groupSize (fst pair) password
+
+groupSize :: Char -> String -> Int
+groupSize digit password = length $ filter (== digit) password
+
+containsPartOfLargerGroup :: String -> Bool
+containsPartOfLargerGroup password = True
 
 passwordsMeetingCriteria :: String -> Int
 passwordsMeetingCriteria range = do
   let parts = wordsWhen (== '-') range
-  let x1 = parts!!0
-  let x2 = parts!!1
+  let x1 = parts !! 0
+  let x2 = parts !! 1
   let p1 = read x1 :: Int
   let p2 = read x2 :: Int
-  length [password | password <- [p1..p2], meetsCriteria (show password)] 
+  length [password | password <- [p1 .. p2], meetsCriteria (show password)]
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =
@@ -18,9 +53,8 @@ wordsWhen p s =
 
 meetsCriteria :: String -> Bool
 meetsCriteria password =
-  length password == 6
-  && neverDecreases password
-  && twoAdjacentDigitsAreSame password
+  length password == 6 &&
+  neverDecreases password && twoAdjacentDigitsAreSame password
 
 twoAdjacentDigitsAreSame :: String -> Bool
 twoAdjacentDigitsAreSame password = any adjacentDigits (pairs password)
--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -15,7 +15,15 @@ day4Examples = TestCase (do
 
 day4 = TestCase (assertEqual "Day4" 1650 (passwordsMeetingCriteria "178416-676461"))
 
-testList = TestList [day4Examples, day4]
+day4Part2Examples = TestCase (do
+  assertEqual "Part 2 Example 1" True (meetsExtendedCriteria "112233")
+  assertEqual "Part 2 Example 2" False (meetsExtendedCriteria "123444")
+  assertEqual "Part 2 Example 3" True (meetsExtendedCriteria "111122")
+  )
+  
+day4Part2 = TestCase (assertEqual "Day4Part2" 0 (passwordsMeetingExtendedCriteria "178416-676461"))
+
+testList = TestList [day4Examples, day4, day4Part2Examples, day4Part2]
 
 main :: IO ()
 main = do
